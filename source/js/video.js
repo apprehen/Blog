fea9419965800fc81b706ffbd0d67e61041a4eb2
Apprenhene <3514392356@qq.com>
2022-10-06 11:57:01 +0800

    提交一下试一试捏

--- source/js/video.js
+++ source/js/video.js
@@ -1,4 +1,8 @@
 const bg = document.getElementById("bg")
-
-
+if(location.href !== 'http://localhost:4000/' && location.href !== 'https://blog.apprehen.space/') {
+  bg.style.display = 'none'
+}
+if (location.href === 'http://localhost:4000/' && location.href === 'https://blog.apprehen.space/'){
+  bg.style.display = 'block'
+}
 
