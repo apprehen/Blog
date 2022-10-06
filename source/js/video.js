f80ec335f174b9a5548122a2877635b184aa3ca2
Apprenhene <3514392356@qq.com>
2022-10-06 12:23:27 +0800

    提交了捏

--- source/js/video.js
+++ source/js/video.js
@@ -1,8 +1,20 @@
 const bg = document.getElementById("bg")
-if(location.href !== 'http://localhost:4000/' && location.href !== 'https://blog.apprehen.space/') {
+function panduan () {
+  let wid = window.innerWidth || window.screen.width || document.body.clientWidth
+  if(location.href !== 'http://localhost:4000/' && location.href !== 'https://blog.apprehen.space/') {
   bg.style.display = 'none'
+  }
+  if (location.href === 'http://localhost:4000/' && location.href === 'https://blog.apprehen.space/'){
+    bg.style.display = 'block'
+  }
+  if (wid < 750){
+    bg.style.display="none"
+  }
+  if (wid > 750) {
+    bg.style.display = "block"
+  }
 }
-if (location.href === 'http://localhost:4000/' && location.href === 'https://blog.apprehen.space/'){
-  bg.style.display = 'block'
+window.onbeforeunload = function () {
+  panduan()
+  console.log("111")
 }
-
