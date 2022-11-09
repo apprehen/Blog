8b45b3ae6d0804a79533f282c8810c07dc7d8aa9
apprehen <3514392356@qq.com>
2022-11-10 00:28:00 +0800

    update

--- themes/butterfly/source/js/search/local-search.js
+++ themes/butterfly/source/js/search/local-search.js
@@ -33,6 +33,7 @@ window.addEventListener('load', () => {
 
   const searchClickFn = () => {
     document.querySelector('#search-button > .search').addEventListener('click', openSearch)
+    document.querySelector('#menu-search').addEventListener('click', openSearch)
   }
 
   const searchClickFnOnce = () => {
