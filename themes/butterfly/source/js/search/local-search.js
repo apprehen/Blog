b0b62527a1aad1a8fc1fc8f7703428cf3ff0a507
apprehen <3514392356@qq.com>
2022-11-10 01:12:47 +0800

    update

--- themes/butterfly/source/js/search/local-search.js
+++ themes/butterfly/source/js/search/local-search.js
@@ -33,7 +33,6 @@ window.addEventListener('load', () => {
 
   const searchClickFn = () => {
     document.querySelector('#search-button > .search').addEventListener('click', openSearch)
-    document.querySelector('#menu-search').addEventListener('click', openSearch)
   }
 
   const searchClickFnOnce = () => {
