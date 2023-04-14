dc72e8d70df1ae3621208420ea254dffb8e4532f
apprehen <3514392356@qq.com>
2023-04-14 10:48:56 +0800

    做了响应式布局适应小屏

--- source/js/live2dpochi/poqi.js
+++ source/js/live2dpochi/poqi.js
@@ -1,49 +1,47 @@
 let cubism4Model = window.location.href + "js/live2dpochi/character/model.json";
-if (window.screen.width > 1000) {
-  (async function main() {
+// if (window.screen.width > 1000) {
+(async function main() {
 
-    const app = new PIXI.Application({
-      view: document.getElementById("canvas"),
-      autoStart: true,
-      resolution: window.devicePixelRatio || 1,
-      // width: 200,
-      // height: 200,
-      antialias: true,
-      transparent: true
-    });
-    const wriper = document.getElementById("live2d-wripe");
-    app.renderer.autoResize = true;
-    app.renderer.resize(wriper.clientWidth, wriper.clientWidth);
-    const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
-    app.stage.addChild(model4);
-    app.stage.width = wriper.clientWidth;
-    app.stage.height = wriper.clientWidth;
-    model4.scale.set(1.0);
-    // model4.position.set(-60, -60);
-    model4.on('hit', async (hitAreas) => {
-      console.log(hitAreas)
-      if (hitAreas.length == 1) {
-        if (hitAreas[0] == '脑袋') {
-          model4.motion('Tap摸头')
-        } else if (hitAreas[0] == '身体') {
-          model4.motion('Tap身体')
-        } else if (hitAreas[0] == '辫子') {
-          model4.motion('Tap摇头')
-        } else {
-          model4.motion('Idle')
-        }
+  const app = new PIXI.Application({
+    view: document.getElementById("canvas"),
+    autoStart: true,
+    resolution: window.devicePixelRatio || 1,
+    // width: 200,
+    // height: 200,
+    antialias: true,
+    transparent: true
+  });
+  const wriper = document.getElementById("live2d-wripe");
+  app.renderer.autoResize = true;
+  app.renderer.resize(wriper.clientWidth, wriper.clientWidth);
+  const model4 = await PIXI.live2d.Live2DModel.from(cubism4Model);
+  app.stage.addChild(model4);
+  app.stage.width = wriper.clientWidth;
+  app.stage.height = wriper.clientWidth;
+  model4.scale.set(1.0);
+  // model4.position.set(-60, -60);
+  model4.on('hit', async (hitAreas) => {
+    console.log(hitAreas)
+    if (hitAreas.length == 1) {
+      if (hitAreas[0] == '脑袋') {
+        model4.motion('Tap摸头')
+      } else if (hitAreas[0] == '身体') {
+        model4.motion('Tap身体')
+      } else if (hitAreas[0] == '辫子') {
+        model4.motion('Tap摇头')
       } else {
-        let random = Math.floor(Math.random() * hitAreas.length)
-        let hitArea = hitAreas[random]
-        console.log(hitArea)
-        if (hitArea === '脑袋') {
-          model4.motion('Tap摸头')
-        } else if (hitArea === '身体') {
-          model4.motion('Tap身体')
-        }
+        model4.motion('Idle')
       }
-    })
-  })();
-} else {
-  console.log('screen too small');
-}
+    } else {
+      let random = Math.floor(Math.random() * hitAreas.length)
+      let hitArea = hitAreas[random]
+      console.log(hitArea)
+      if (hitArea === '脑袋') {
+        model4.motion('Tap摸头')
+      } else if (hitArea === '身体') {
+        model4.motion('Tap身体')
+      }
+    }
+  })
+})();
+
